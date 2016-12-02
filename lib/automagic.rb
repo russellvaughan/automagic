11bffebd38d99c730797f7e41b87d29bac250b5d
Russell Vaughan <russell@gosquared.com>
2016-12-02 18:34:06 +0000

    workign on it

--- lib/automagic.rb
+++ lib/automagic.rb
@@ -1,41 +1,64 @@
 require 'csv'
 
-  def import(csv)
-      @csv = CSV.read(csv)
-      @headers = @csv.first
-      @headers.pop while @headers.last.nil? && @headers.size>0
-  end
+def import(csv)
+  @csv = CSV.read(csv)
+  @headers = @csv.first
+  @headers.pop while @headers.last.nil? && @headers.size>0
+end
 
-  def map_data
-    @data = {}
-    x = 0
-    @csv.each do |row|
-       @properties = {}
-      row.length.times do |number|
-        @headers.length.times do |num|
-        @properties[@headers[num]] = row[num]
-        end
+def map_data
+  @data = {}
+  x = 0
+  @csv.each do |row|
+   @properties = {}
+   row.length.times do |number|
+    @headers.length.times do |num|
+      @properties[@headers[num]] = row[num]
+    end
+  end
+  @data[x]=@properties
+  x+=1
+end
+end
+
+
+ gs.tracking.identify({
+  person_id: "email:user@test.com", # Required
+
+  # Reserved property names
+  properties: {
+    name: "Test User",
+    username: "testuser",
+    phone: "+44123456789",
+    created_at:"2016-06-07T15:44:20Z", # ISO 8601 formatted String
+    company_name:"GoSquared",
+    company_industry:"Customer Analytics",
+    company_size: 15000,
+
+    # Custom properties
+    custom: {
+      # custom_property_name: "custom property value"  # You can track as many custom properties as you like
+    }
+  }
+  })
+
+ def  match_standard_props
+  @headers.each do |header|
+    @property_fields.each do |property|
+      if header.downcase == property
+        @standard_props << header
       end
-      @data[x]=@properties
-     x+=1
     end
   end
+end
 
 
-  property_fields = ['id', 'email', 'name', 'first_name', 'last_name',
-      'username', 'phone', 'created_at']
 
-
- def first_row_values
-  @values = @csv[1]
-  @values.pop while @values.last.nil? && @values.size>0
- end
-
-    end
-    end
-  end
-  end
-  end
+  #   end
+  #   end
+  # end
+  # end
+  # end
 
 
       # properties = {}
@@ -47,22 +70,22 @@ require 'csv'
       #       properties['totals'] = @totals
       #     end
       #   end
-      # end
+#       # end
 
-standard_properties = ['id', 'email', 'name', 'first_name', 'last_name', 'user_name', 'phone', 'created_at']
-csv = CSV.read('dummy_data.csv')
-properties = {}
-hash = []
+@standard_properties = ['id', 'email', 'name', 'first_name', 'last_name', 'user_name', 'phone', 'created_at']
+# csv = CSV.read('dummy_data.csv')
+# properties = {}
+# hash = []
 
 
-values = csv[1]
-x = 0
-standard_properties.each  do |property|
-properties[property]  = values[x]
-x+=1
-hash << properties
-end
+# values = csv[1]
+# x = 0
+# standard_properties.each  do |property|
+# properties[property]  = values[x]
+# x+=1
+# hash << properties
+# end
 
-end
+# end
 
-end
+# end
